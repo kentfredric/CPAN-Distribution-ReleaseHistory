276c69858b96363c841dc82849aed5f50665c1c0
Kent Fredric <kentfredric@gmail.com>
2014-11-13 05:49:41 +1300

    Build results of 847e5e3 (on master)

--- lib/CPAN/Distribution/ReleaseHistory.pm
+++ lib/CPAN/Distribution/ReleaseHistory.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package CPAN::Distribution::ReleaseHistory;
 
-our $VERSION = '0.002002';
+our $VERSION = '0.002003';
 
 # ABSTRACT: Show the release history of a single distribution
 
@@ -213,7 +213,7 @@ CPAN::Distribution::ReleaseHistory - Show the release history of a single distri
 
 =head1 VERSION
 
-version 0.002002
+version 0.002003
 
 =head1 SYNOPSIS
 
